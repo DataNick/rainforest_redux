1b91dd979a1804cf08638182b3086cca23c75bc7
DataNick <DataNick@users.noreply.github.com>
2015-03-30 04:53:01 -0400

    Creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
 
 
 
+  # get 'sessions/new'
+
+  # get 'sessions/create'
+
+  # get 'sessions/destroy'
+
   root 'welcome#index'
   resources :products
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 
   # get 'products/index'
