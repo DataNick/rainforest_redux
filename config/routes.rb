b84bb40443d5a04e469be2efc4bdd657098a8f7b
DataNick <DataNick@users.noreply.github.com>
2015-03-30 05:23:31 -0400

    Added show route and view for user

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   resources :products
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 
 
