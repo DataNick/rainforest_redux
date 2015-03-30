0522bd1d66badb5aa79ae113acdbeec342e4442d
DataNick <DataNick@users.noreply.github.com>
2015-03-30 17:35:23 -0400

    created reviews model and associations across all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	has_many :reviews
+	has_many :products, through: :reviews
 	has_secure_password
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328040621) do
+ActiveRecord::Schema.define(version: 20150330213035) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "price_in_cents"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.text     "comment"
+    t.integer  "product_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
