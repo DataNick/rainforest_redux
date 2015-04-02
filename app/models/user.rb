1712f568e2ab7ea6229ffc1c60b2afbf6388e20f
DataNick <DataNick@users.noreply.github.com>
2015-04-01 23:06:11 -0400

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :reviews
 	has_many :products, through: :reviews
+	validates :name, presence: :true
 	has_secure_password
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330213035) do
+ActiveRecord::Schema.define(version: 20150402015914) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150330213035) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
