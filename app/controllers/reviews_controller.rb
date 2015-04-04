67e96e99b762fe859dbac7cdf195e309f2d055ea
DataNick <DataNick@users.noreply.github.com>
2015-04-04 15:09:24 -0400

    In destroy action, redirect to products_path after deletion

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,6 +18,7 @@ class ReviewsController < ApplicationController
   def destroy
   	@review = Review.find(params[:id])
   	@review.destroy
+    redirect_to products_path, notice: 'Review was deleted'
   end
 
   def edit
