ccb14b56dc377a362970680c0f67bc6ed26491b8
DataNick <DataNick@users.noreply.github.com>
2015-04-21 23:57:13 -0400

    Commented out vanilla javascript, visited web site to check functionality with jQuery notation

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,28 +2,28 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(document).ready(function() {
-	function display_search_results() {
-		// readyState === 4 means that the asynchronous request completed successfully
-		if (this.readyState === 4) {
-			console.log(this);
-			document.getElementById('products').innerHTML = this.responseText;
-		}
-	}
+// $(document).ready(function() {
+// 	function display_search_results() {
+// 		// readyState === 4 means that the asynchronous request completed successfully
+// 		if (this.readyState === 4) {
+// 			console.log(this);
+// 			document.getElementById('products').innerHTML = this.responseText;
+// 		}
+// 	}
 
-	var form = document.getElementById('search_form');
-	form.addEventListener('submit', function(event) {
-		event.preventDefault();
-		var searchValue = document.getElementById('search').value;
+// 	var form = document.getElementById('search_form');
+// 	form.addEventListener('submit', function(event) {
+// 		event.preventDefault();
+// 		var searchValue = document.getElementById('search').value;
 
-		var xhr = new XMLHttpRequest();
-		xhr.onload = display_search_results;
-		xhr.open('GET', '/products?search=' + searchValue, true);
-		xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest"); // X-Requested-With, XMLHttpRequest
-		// is a special header that makes the method .xhr? available
-		xhr.send();
-	});
-});
+// 		var xhr = new XMLHttpRequest();
+// 		xhr.onload = display_search_results;
+// 		xhr.open('GET', '/products?search=' + searchValue, true);
+// 		xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest"); // X-Requested-With, XMLHttpRequest
+// 		// is a special header that makes the method .xhr? available
+// 		xhr.send();
+// 	});
+// });
 
 $(document).on('ready page:load', function() {
 	$('#search_form').submit(function(event){
