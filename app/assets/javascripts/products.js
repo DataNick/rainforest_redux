d8b32b5f289206b27628c3de05330c2535ed0aa3
DataNick <DataNick@users.noreply.github.com>
2015-04-22 00:01:56 -0400

    Removed $.ajax and replaced it with $.get which allows for the writing of less code

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -30,13 +30,18 @@ $(document).on('ready page:load', function() {
 		event.preventDefault();
 		var searchValue = $('#search').val();
 
-		$.ajax({
-			url: '/products?search=' + searchValue,
-			type: 'GET',
-			dataType: 'html'
-		}).done(function(data){
-			$('#products').html(data);
-		});
+		// $.ajax({
+		// 	url: '/products?search=' + searchValue,
+		// 	type: 'GET',
+		// 	dataType: 'html'
+		// }).done(function(data){
+		// 	$('#products').html(data);
+		// });
+		$.get('/products?search=' + searchValue)
+			.done(function(data){
+				console.log(data);
+				$('#products').html(data);
+			});
 	});
 });
 
