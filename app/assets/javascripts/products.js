9ae6483eb2172867b86581c2b02cf1f8fe7724a9
DataNick <DataNick@users.noreply.github.com>
2015-04-23 14:21:41 -0400

    Placed console.log before returning $.getScript to check how pagination works

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -57,8 +57,8 @@ $(document).on('ready page:load', function() {
 		var url = $('.pagination span.next').children().attr('href');
 		if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
 			$('.pagination').text("Fetching more products...");
-			return $.getScript(url);
 			console.log("Paginate all the things");
+			return $.getScript(url);
 		}
 	});
 }
