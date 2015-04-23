23f4eb8d9ff3466a8d4cf7d134a760f47842209d
DataNick <DataNick@users.noreply.github.com>
2015-04-22 21:31:37 -0400

    Trying to use search and pagination functions

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -50,3 +50,31 @@ $(document).on('ready page:load', function() {
 	});
 });
 
+
+$(document).on('ready page: load', function(){
+	if ($('.pagination').length) {
+	$(window).scroll(function(){
+		var url = $('.pagination span.next').children().attr('href');
+		if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+			$('.pagination').text("Fetching more products...");
+			return $.getScript(url);
+		}
+	});
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
