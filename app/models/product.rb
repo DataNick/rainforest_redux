d27511af9b829d487e8feb1293f89ecf00d8adf3
DataNick <DataNick@users.noreply.github.com>
2015-04-24 22:02:03 -0400

    Added belongs_to category association to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
 	has_many :reviews, :dependent => :destroy
 	has_many :users, through: :reviews
+	belongs_to :category
 	validates :description, :name, presence: true
 	validates :price_in_cents, numericality: {only_integer: true}
 
