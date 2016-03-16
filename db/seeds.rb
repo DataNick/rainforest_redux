36caab881f88e38fd19ca51835b2722fbc750a55
DataNick <DataNick@users.noreply.github.com>
2016-03-16 12:13:11 -0400

    Removed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-50.times do
-	Product.create(
-		name: Faker::Company.catch_phrase,
-		description: "#{Faker::Company.bs} from #{Faker::Company.name} #{Faker::Company.suffix}",
-		price_in_cents: rand(100000)
-		)
-end
