fab9deeea12ee51cf99eedaec3088216d3a6993c
DataNick <DataNick@users.noreply.github.com>
2016-03-24 16:12:15 -0400

    Fixed spacing issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
-	has_many :reviews
-	has_many :products, through: :reviews
-	validates :name, presence: :true
-	has_secure_password
-
-
+  has_many :reviews
+  has_many :products, through: :reviews
+  validates :name, presence: :true
+  has_secure_password
 end
