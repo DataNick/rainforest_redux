b88207bab4d1590079584564e643e213bda00807
DataNick <DataNick@users.noreply.github.com>
2016-03-24 16:11:26 -0400

    Fixed spacing issues

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,13 @@
 class Product < ActiveRecord::Base
-	has_many :reviews, :dependent => :destroy
-	has_many :users, through: :reviews
-	belongs_to :category
-	validates :description, :name, presence: true
-	validates :price_in_cents, numericality: {only_integer: true}
+  has_many :reviews, :dependent => :destroy
+  has_many :users, through: :reviews
+  belongs_to :category
+  validates :description, :name, presence: true
+  validates :price_in_cents, numericality: {only_integer: true}
 
-	def formatted_price
-		price_in_dollars = price_in_cents.to_f / 100
-		sprintf("%.2f", price_in_dollars)
-	end
+  def formatted_price
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf("%.2f", price_in_dollars)
+  end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :product
-	validates :comment, :presence => true
+  belongs_to :user
+  belongs_to :product
+  validates :comment, :presence => true
 end
